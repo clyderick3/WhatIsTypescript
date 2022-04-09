b66eb837adab2f11792aa3c20f957d43639f7385
Clyde Broderick <clyderick3@gmail.com>
2022-04-09 18:55:04 -0400

    Cleaned up some old code.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,32 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+
+//   const form: HTMLFormElement = document.querySelector('#defineform');
+//   document.body.addEventListener("submit", async function(event){
+//   event.preventDefault();
+//   const form = event.target as HTMLFormElement;
+//       });
+
+// form.onsubmit = () => {
+//   const formData = new FormData(form);
+//   console.log(formData);
+//   const text = formData.get('defineword') as string;
+//   fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + text)
+//       .then((response: Response) => response.json())
+//       .then(function(data){console.log(data)})
+//       .catch((error) => console.log(error));
+//   console.log(text);
+//   return true; // prevent reload
+
+// };
+//   return (
+//       <div className="App">
+         
+//       </div>
+//   );
+// }
+// export default App;
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { render } from "@testing-library/react";
+const axios = require('axios');
 
 function App() {
 
-  const form: HTMLFormElement = document.querySelector('#defineform');
-  document.body.addEventListener("submit", async function(event){
-  event.preventDefault();
-  const form = event.target as HTMLFormElement;
-      });
+  const [def, setDef] = useState([]);
 
-form.onsubmit = () => {
-  const formData = new FormData(form);
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + text)
-      .then((response: Response) => response.json())
-      .then(function(data){console.log(data)})
-      .catch((error) => console.log(error));
-  console.log(text);
-  return true; // prevent reload
 
-};
-  return (
-      <div className="App">
-         
-      </div>
-  );
-}
 
-export default App;
+  const form: HTMLFormElement = document.querySelector("#defineform");
+  document.body.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    let form = event.target as HTMLFormElement;
+  });
+
+
+
+    useEffect(() => {
+      const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+      form.onsubmit = async () => {
+        const formData = new FormData(form);
+        // console.log(formData);
+        const text = formData.get("defineword") as string;
+        axios.get(url + text)
+          .then((res) => {
+            console.log(res);
+            setDef(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+        })
+        //.then((response: Response) => response.json())
+        // const json = response.json();
+        // setDef(await json);
+        //.then(function (data) {
+        // console.log(json);
+        //  console.log(def);
+      }
+    }
+
+      //.catch((error) => console.log(error));
+      //console.log(text);
+
+      ,[]);
+<main className="container">
+
+          <div className="bg-light p-5 rounded">
+            <h1 className="defined">Definition</h1>
+            <p className="lead">
+            Random Text
+            </p>
+            {def.map((def) => (
+            <ul className="list-unstyled">
+              <li>{def.word}</li>
+                <li>Random Text</li>
+              <li>Maybe your multiple definitions of the word are here?</li>
+              <li>Structurally, it's still a list.</li>
+              <li>
+                However, this style only applies to immediate child elements.
+              </li>
+              <li>
+                Nested lists: (maybe synonyms and antonyms?)
+                <ul>
+                  <li>are unaffected by this style</li>
+                  <li>will still show a bullet</li>
+                  <li>and have appropriate left margin</li>
+                </ul>
+              </li>
+              <li>This may still come in handy in some situations.</li>
+              </ul>
+              ))}
+           </div>
+
+        </main>
