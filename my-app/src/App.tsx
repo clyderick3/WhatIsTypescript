8e966f00a8756719ced38d1bfeee028917de67e3
Clyde Broderick <clyderick3@gmail.com>
2022-04-10 22:35:21 -0400

    Fixed warnings. Definitions are working.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -67,47 +65,7 @@ function App() {
 
 </main>
     </div>
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-
->>>>>>> parent of b66eb83 (Cleaned up some old code.)
-  const form: HTMLFormElement = document.querySelector('#defineform');
-  document.body.addEventListener("submit", async function(event){
-  event.preventDefault();
-  const form = event.target as HTMLFormElement;
-      });
-
-form.onsubmit = () => {
-  const formData = new FormData(form);
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + text)
-      .then((response: Response) => response.json())
-      .then(function(data){console.log(data)})
-      .catch((error) => console.log(error));
-  console.log(text);
-  return true; // prevent reload
-
-};
-  return (
-      <div className="App">
-         
-      </div>
-<<<<<<< HEAD
->>>>>>> parent of b66eb83 (Cleaned up some old code.)
-=======
->>>>>>> parent of b66eb83 (Cleaned up some old code.)
   );
 }
 
